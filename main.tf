60dda8b77a2d1077e41b84c3668ee4f8cb9149c3
devops-aws-vinay <104916095+devops-aws-vinay@users.noreply.github.com>
2024-03-14 16:17:24 +0530

    push

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_vpc" "demo" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc-cidrip
   instance_tenancy = "default"
 }
 
 resource "aws_subnet" "sub1a" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.sub1a-ip
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub1b" {
   vpc_id     = aws_vpc.demo.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.sub1b-ip
   availability_zone = "ap-south-1b"
   map_public_ip_on_launch = true
 }
