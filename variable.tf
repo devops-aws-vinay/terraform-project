e611bab69326b7fd30a17e38abad17ca43460eef
devops-aws-vinay <104916095+devops-aws-vinay@users.noreply.github.com>
2024-03-14 15:21:42 +0530

    push

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
+variable "vpc-cidrip"{
+    default = "10.0.0.0/16"
+}
 
+variable "sub1a-ip"{
+    default = "10.0.1.0/24"
+}
+
+variable "sub1b-ip"{
+    default = "10.0.2.0/24"
+}
+
+variable "vpc-sg"{
+    default = [aws_security_group.vpcsg.id]
+}
